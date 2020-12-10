12b0ba14baaee92247b7c8157e83834bd40fd119
Evan Duru <eviedee007@yahoo.co.uk>
2020-12-10 18:12:34 +0000

    updating articles working

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,12 +9,33 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
+
+  end
+
+  def edit
+    @article = Article.find(params[:id])
 
   end
 
   def create
-    render plain: params[:article]
-
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = 'Article was created succesfully.'
+    redirect_to @article
+    else
+      render 'new'
+    end
   end
+
+def update
+  @article = Article.find(params[:id])
+  if @article.update(params.require(:article).permit(:title, :description))
+    flash[:notice] = "Article was updated succesfully."
+    redirect_to @article 
+    else
+      render "edit"
+  end 
+end
 
 end
